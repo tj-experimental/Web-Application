af47219819f37907f556bb93a6976b36a9b7bead
Tonye Jack <jtonye@ymail.com>
2016-09-04 20:46:16 -0400

    Changed to https module

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-const https = require('https');
+const http = require('http');
 //const open = require('open');
 var requestHandler = require('./requestHandler.js');
 
-
 var port  = process.env.PORT || process.argv[2] || 8080;
 
-
-https.createServer(function(req, res){
+http.createServer(function(req, res){
     requestHandler.formRequest(req, res);
     requestHandler.home(req, res);
 
 }).listen(port);
 
-console.log("Started Server on port http://mysite:" + port);
+console.log("Started Server on port http://localhost:" + port);
 // open('http://mysite:' +port, 'chrome');
 // process.on('SIGINT', function() {
 //     console.log("Caught interrupt signal CTRl^C");
