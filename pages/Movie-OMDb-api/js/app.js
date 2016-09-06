e23158c5b92b6d0625a980b394a2708d8254a811
Tonye Jack <jtonye@ymail.com>
2016-09-05 22:05:42 -0400

    changed http to https for omdb api

--- pages/Movie-OMDb-api/js/app.js
+++ pages/Movie-OMDb-api/js/app.js
@@ -117,4 +117,8 @@ $(function () {
 
     });
 
+    if(window.location.protocol != "https:"){
+        window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+    }
+
 });
