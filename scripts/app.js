efe1b080e1377ce0f898a50e44608bb5351cc245
Tonye Jack <jtonye@ymail.com>
2017-10-26 16:36:03 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,60 +1,3 @@
-var userModule = angular.module("WebApp", []);
-userModule.controller("userCtrl",['$scope', '$http', function($scope, $http){
-		  $http.get('chart/user.json')
-            .success(function (userData) {
-                $scope.user = userData;
-            }).error(function (userData, status, error, config) {
-              	console.error("Error retrieving user data", error, status);
-            });
-}]);
-
-userModule.controller("emailCtrl",['$scope', function($scope){
-		$scope.email= {
-			emailAdd: "",
-			pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,
-			emailPlaceHolder1: "abc@xyz.com",
-			emailPlaceHolder2: "E-mail Address"
-		};
-		$scope.fields = [''];
-}]);
-
-userModule.controller("planCtrl", ['$scope', function ($scope) {
-	     $scope.plans = [
-	     	{id: "pers", planName: "Personal", descriptionId: "personal_plan"},
-			{id: "agn", planName: "Agency", descriptionId: "agency_plan"},
-			 {id: "unl", planName: "Unlimited", descriptionId: "unlimited_plan"}
-		 ]
-}]);
-
-
-
-userModule.controller("projectCtrl", ['$scope', function ($scope) {
-	   $scope.projects = [
-		   { link:"https://chat-app0.herokuapp.com", name: "Messenger App"},
-		   { link:"pages/Movie-OMDb-api/index.html", name: "OMDb MovieApp"},
-		   { link:"pages/geolocation/index.html", name: "Geolocation App"},
-		   { link:"pages/flickrAPI_searchForm/index.html", name: "Flickr SearchApp"},
-		   { link:"pages/flickrapi/index.html", name: "Flickr ImageApp"},
-		   { link:"pages/workopolisAPI/index.html", name: "Workopolis API"},
-		   { link:"pages/Interactive-Form/index.html", name: "Payment Form"},
-		   { link:"pages/Object-prototype-JavaScript/Quiz-OO-Javascript/index.html", name: "Quiz App"},
-		   { link: "pages/Object-prototype-JavaScript/OO-JavaScript-Media-Player/index.html", name: "Media PlayerApp"},
-		   { link: "pages/Pagination-Content-Filter/index.html", name: "PaginationApp"},
-		   { link: "pages/tic-tac-toe/index.html", name: "Tic Tac Toe App"},
-		   { link: "pages/Address-book/index.html", name: "Address Book App"},
-		   { link: "pages/angularApp/index.html", name:"Angular App"}
-	   	]
-}]);
-
-
-userModule.controller("footerCtrl", ['$scope', function ($scope) {
-        $scope.items = ['Gadget', 'Science', 'Nature', 'Creative'];
-        $scope.location = "#";
-        $scope.className = "url";
-        $scope.imageUrl = "images/box_image.jpg";
-
-}]);
-
 var graphModule = angular.module("GraphApp", ['n3-line-chart']);
 
 graphModule.controller("graphCtrl", ['$scope', '$http', function($scope, $http){
@@ -122,8 +65,66 @@ graphModule.controller("graphCtrl", ['$scope', '$http', function($scope, $http){
 }]);
 
 
+var userModule = angular.module("WebApp", ['GraphApp']);
+
+userModule.controller("userCtrl",['$scope', '$http', function($scope, $http){
+		  $http.get('chart/user.json')
+            .success(function (userData) {
+                $scope.user = userData;
+            }).error(function (userData, status, error, config) {
+              	console.error("Error retrieving user data", error, status);
+            });
+}]);
+
+userModule.controller("emailCtrl",['$scope', function($scope){
+		$scope.email= {
+			emailAdd: "",
+			pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,
+			emailPlaceHolder1: "abc@xyz.com",
+			emailPlaceHolder2: "E-mail Address"
+		};
+		$scope.fields = [''];
+}]);
+
+userModule.controller("planCtrl", ['$scope', function ($scope) {
+	     $scope.plans = [
+	     	{id: "pers", planName: "Personal", descriptionId: "personal_plan"},
+			{id: "agn", planName: "Agency", descriptionId: "agency_plan"},
+			 {id: "unl", planName: "Unlimited", descriptionId: "unlimited_plan"}
+		 ]
+}]);
+
+
+
+userModule.controller("projectCtrl", ['$scope', function ($scope) {
+	   $scope.projects = [
+		   { link:"https://chat-app0.herokuapp.com", name: "Messenger App"},
+		   { link:"pages/Movie-OMDb-api/index.html", name: "OMDb MovieApp"},
+		   { link:"pages/geolocation/index.html", name: "Geolocation App"},
+		   { link:"pages/flickrAPI_searchForm/index.html", name: "Flickr SearchApp"},
+		   { link:"pages/flickrapi/index.html", name: "Flickr ImageApp"},
+		   { link:"pages/workopolisAPI/index.html", name: "Workopolis API"},
+		   { link:"pages/Interactive-Form/index.html", name: "Payment Form"},
+		   { link:"pages/Object-prototype-JavaScript/Quiz-OO-Javascript/index.html", name: "Quiz App"},
+		   { link: "pages/Object-prototype-JavaScript/OO-JavaScript-Media-Player/index.html", name: "Media PlayerApp"},
+		   { link: "pages/Pagination-Content-Filter/index.html", name: "PaginationApp"},
+		   { link: "pages/tic-tac-toe/index.html", name: "Tic Tac Toe App"},
+		   { link: "pages/Address-book/index.html", name: "Address Book App"},
+		   { link: "pages/angularApp/index.html", name:"Angular App"}
+	   	]
+}]);
+
+
+userModule.controller("footerCtrl", ['$scope', function ($scope) {
+        $scope.items = ['Gadget', 'Science', 'Nature', 'Creative'];
+        $scope.location = "#";
+        $scope.className = "url";
+        $scope.imageUrl = "images/box_image.jpg";
+
+}]);
+
+
 angular.element(document).ready(function(){
-	angular.bootstrap(document.getElementById("chart"),['GraphApp']);
 	setTimeout(function () {
 		$('.users').slick({
 			infinite: true,
