ae3435b868abe4c54d4cb477a32b21cc47d40a14
Tonye Jack <jtonye@ymail.com>
2017-11-01 02:24:03 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,93 +1,93 @@
-var graphModule = angular.module("GraphApp", ['n3-line-chart']);
+// var graphModule = angular.module("GraphApp", ['n3-line-chart']);
 
-graphModule.controller("graphCtrl", ['$scope', function($scope, $http){
-	    d3.json('chart/chart.json', function(error, data){
-	        var dateToInt = function (monthStr){
-		    return new Date(monthStr+'-1-01').getMonth()+1
-		}
-		$scope.data = {
-			dataset: data[0].numbers
-		};
-		$scope.options = {
-		   tooltipHook: function(d){
-			 if(d){
-				return {
-					abscissas: "Social Revenue Impact",
-					rows:  d.map(function(s){
-						return {
-							label: "New " + s.series.label,
-							value: s.row.y1,
-							color: s.series.color,
-							id: s.series.id 
-							}
-						})
-					}
-				}
-		      },
-		   series: [
-			{
-		          axis: "y", 
-			  dataset: "dataset",
-			  key: {y0: "money", y1: "like"}, 
-			  label: "Money", 
-			  type: ['line', 'dot', 'area'],
-			  interpolation: {mode: 'cardinal', tension: 0.7},
-			  defined: function(value) {
-			      return value.y1 !== undefined;
-			  },
-			  id: "mySeries0",
-			  visible: true,
-			  color: "rgb(126, 181, 63)"
-			}
-// 			,
+// graphModule.controller("graphCtrl", ['$scope', function($scope, $http){
+// 	    d3.json('chart/chart.json', function(error, data){
+// 	        var dateToInt = function (monthStr){
+// 		    return new Date(monthStr+'-1-01').getMonth()+1
+// 		}
+// 		$scope.data = {
+// 			dataset: data[0].numbers
+// 		};
+// 		$scope.options = {
+// 		   tooltipHook: function(d){
+// 			 if(d){
+// 				return {
+// 					abscissas: "Social Revenue Impact",
+// 					rows:  d.map(function(s){
+// 						return {
+// 							label: "New " + s.series.label,
+// 							value: s.row.y1,
+// 							color: s.series.color,
+// 							id: s.series.id 
+// 							}
+// 						})
+// 					}
+// 				}
+// 		      },
+// 		   series: [
 // 			{
-// 		  	  axis: "y",
+// 		          axis: "y", 
 // 			  dataset: "dataset",
-// 			  key: {y0: "like", y1: "views", y2: "share"},
+// 			  key: {y0: "money", y1: "like"}, 
+// 			  label: "Money", 
 // 			  type: ['line', 'dot', 'area'],
-// 			  label: "Social presence",
-// 			  color: "rgb(200, 96, 69)",
+// 			  interpolation: {mode: 'cardinal', tension: 0.7},
+// 			  defined: function(value) {
+// 			      return value.y1 !== undefined;
+// 			  },
+// 			  id: "mySeries0",
 // 			  visible: true,
-// 			  id: "social",
+// 			  color: "rgb(126, 181, 63)"
 // 			}
-// 			,{
-// 		          axis: 'y',
-// 		          id: 'views0',
-// 			  dataset: "dataset",
-// 			  key: 'views',
-// 			  type: ['line', 'dot', 'area'],
-// 			  label: 'Views',
-// 			  color: "rgb(193, 92, 69)"
-// 			},
-// 			{
-// 			  axis: 'y',
-// 			  dataset: "dataset",
-// 			  key: 'share',
-// 			  type: ['line', 'dot', 'area'],
-// 			  label: 'Share',
-// 		          id: "share0",
-// 			  color: "rgb(119, 48, 131)"
-// 			}
-		     ],
-		     axes: {
-			  x:{ key: "month",
-			      type: 'linear'
-// 			     ,
-// 			      tickFormat: function(d, i){ 
-// 			          return dateToInt(d);
-// 			      }
-			  },
-			  y:{ min: 0, max: 11000 },
-		          y2: {min: 0, max: 12000 }
-		    }
-		};
-	       $scope.$apply();
-	       console.dir($scope.data);
-	    });
-}]);
+// // 			,
+// // 			{
+// // 		  	  axis: "y",
+// // 			  dataset: "dataset",
+// // 			  key: {y0: "like", y1: "views", y2: "share"},
+// // 			  type: ['line', 'dot', 'area'],
+// // 			  label: "Social presence",
+// // 			  color: "rgb(200, 96, 69)",
+// // 			  visible: true,
+// // 			  id: "social",
+// // 			}
+// // 			,{
+// // 		          axis: 'y',
+// // 		          id: 'views0',
+// // 			  dataset: "dataset",
+// // 			  key: 'views',
+// // 			  type: ['line', 'dot', 'area'],
+// // 			  label: 'Views',
+// // 			  color: "rgb(193, 92, 69)"
+// // 			},
+// // 			{
+// // 			  axis: 'y',
+// // 			  dataset: "dataset",
+// // 			  key: 'share',
+// // 			  type: ['line', 'dot', 'area'],
+// // 			  label: 'Share',
+// // 		          id: "share0",
+// // 			  color: "rgb(119, 48, 131)"
+// // 			}
+// 		     ],
+// 		     axes: {
+// 			  x:{ key: "month",
+// 			      type: 'linear'
+// // 			     ,
+// // 			      tickFormat: function(d, i){ 
+// // 			          return dateToInt(d);
+// // 			      }
+// 			  },
+// 			  y:{ min: 0, max: 11000 },
+// 		          y2: {min: 0, max: 12000 }
+// 		    }
+// 		};
+// 	       $scope.$apply();
+// 	       console.dir($scope.data);
+// 	    });
+// }]);
 
 
-var userModule = angular.module("WebApp", ['GraphApp']);
+var userModule = angular.module("WebApp", []);
 
 userModule.controller("userCtrl",['$scope', function($scope){
 		  d3.json('chart/user.json', function (error, data) {
